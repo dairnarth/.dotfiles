7c71ca745d594c811dda0f917c123b5f4ecf7bac
Dylan Cairns-Howarth <dpch@hotmail.co.uk>
2023-04-15 00:28:19 +0100

    fix(qtile): Fix battery widget

--- stow/qtile/.config/qtile/barconfig.py
+++ stow/qtile/.config/qtile/barconfig.py
@@ -2,6 +2,16 @@ import os
 from libqtile import bar, widget
 from modules.windowname import WindowName
 
+def battery():
+    def _inner():
+        b = open('/sys/class/power_supply/BAT0/capacity', 'r').read().replace('\n', '').zfill(3)
+        if int(b) <= 25:
+            return "<span color='#eb5b4b'> {}</span>".format(b)
+        else:
+            return " {}".format(b)
+
+    return _inner
+
 def init_bar(active=False):
     widgets = [
         widget.CurrentLayoutIcon(custom_icon_paths = ["/home/dch/.config/qtile/icons"]),
@@ -19,6 +29,10 @@ def init_bar(active=False):
             widget.Clock(format=" %y%m%d %H%M%S"),
         ])
         if os.path.exists("/sys/class/power_supply/BAT0/capacity"):
-            widgets.append(widget.Battery(format=" {percent:3.0}"))
+            widgets.append(widget.GenPollText(
+                               func=battery(),
+                               update_interval=10,
+                            )
+            )
 
     return bar.Bar(widgets, 16)
