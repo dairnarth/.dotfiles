0c5ecd61981ade6319e7f4ad6252eb33b03e9ff6
Dylan Cairns-Howarth <dpch@hotmail.co.uk>
2023-12-22 00:11:16 +0000

    feat(fish): Unify modal cursor shape with nvim
    Also removed duplicate ssh function incorrectly named tmux.fish.

--- stow/fish/.config/fish/conf.d/00 Environment.fish
+++ stow/fish/.config/fish/conf.d/00 Environment.fish
@@ -4,4 +4,7 @@ set -gx TERMINAL "kitty"
 set -gx BROWSER "qutebrowser"
 
 set -g fish_greeting
+set -g fish_cursor_insert line
+set -g fish_cursor_replace_one underscore
+set -g fish_cursor_visual block
 fish_vi_key_bindings
