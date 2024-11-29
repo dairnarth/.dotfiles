57f501074d9f672c9b999664d670ec06f792e1d8
Dylan Cairns-Howarth <dpch@hotmail.co.uk>
2024-11-29 03:22:50 +0000

    fix(fish/abc2pdf): Stop accidental creation of extra file

--- stow/fish/.config/fish/functions/abc2pdf.fish
+++ stow/fish/.config/fish/functions/abc2pdf.fish
@@ -2,10 +2,10 @@ function abc2pdf
     set -l portrait (mktemp)
     set -l landscape (mktemp)
 
-    set -l perr (abcm2ps -O  $portrait  $argv[1] 2>&1 | wc -l)
-    set -l lerr (abcm2ps -lO $landscape $argv[1] 2>&1 | wc -l)
+    set -l perr (abcm2ps -O  $portrait  $argv[1] &| wc -l)
+    set -l lerr (abcm2ps -lO $landscape $argv[1] &| wc -l)
 
-    if test $perr > $lerr
+    if test $perr -gt $lerr
         ps2pdf $landscape -
     else
         ps2pdf $portrait -
