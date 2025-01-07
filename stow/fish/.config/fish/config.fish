0adbe3412a9799f0fbedd26527fce6c4f17d3937
Dylan Cairns-Howarth <dpch@hotmail.co.uk>
2025-01-07 02:10:16 +0000

    refactor(tmux/fish): Configure tmux in fish startup
    As I exclusively use the terminal in tmux these days, it makes very
    little sense not to have my shell start and configure it.
    Also switch from `htop` to `btop`.

--- stow/fish/.config/fish/config.fish
+++ stow/fish/.config/fish/config.fish
@@ -1,5 +1,13 @@
 if status is-interactive
     if not set -q TMUX
-        exec tmux-default
+        if tmux attach-session -t default 2>/dev/null
+            exit 0
+        else
+            tmux new -s default -d
+            tmux rename-window -t default sys-mon
+            tmux send-keys -t default 'btop' Enter
+            tmux new-window -t default
+            exec tmux attach-session -t default
+        end
     end
 end
